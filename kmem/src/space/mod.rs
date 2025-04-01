18e8d7a1229e349e825d3a08c8635441b2eca29e
周睿 <zrufo747@outlook.com>
2025-04-01 17:47:09 +0800

    update

--- kmem/src/space/mod.rs
+++ kmem/src/space/mod.rs
@@ -1,3 +1,18 @@
-pub(crate) static mut KCODE_VA_OFFSET: usize = 0;
+bitflags::bitflags! {
+    #[repr(transparent)]
+    #[derive(Clone, Copy, PartialEq, Eq)]
+    pub struct AccessFlags: u8 {
+        const Read = 1;
+        const Write = 1 << 2;
+        const LowerPrivilege = 1 << 3;
+    }
+}
 
-
+#[repr(u8)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum CacheSetting {
+    Normal,
+    NonCache,
+    WriteBack,
+    WriteThrough,
+}
