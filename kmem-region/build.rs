292fe518afa7f26e14184bec9ddb06487218613f
周睿 <zrufo747@outlook.com>
2025-05-16 17:07:20 +0800

    update el2 addr

--- kmem-region/build.rs
+++ kmem-region/build.rs
@@ -28,9 +28,9 @@ fn main() {
 
     if std::env::var("CARGO_FEATURE_SPACE_LO").is_ok() {
         addr_base = 0;
-        // kernel_load_vaddr = (1 << addr_bits) / 16 * 15 + 0x200000;
+        kernel_load_vaddr = (1 << addr_bits) / 16 * 15 + 0x200000;
 
-        kernel_load_vaddr = 0x40200000;
+        // kernel_load_vaddr = 0x40200000;
     }
 
     let const_content = quote! {
