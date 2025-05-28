a9c9a5e3d8ec7822d88493c5db61a45edaf1cdc8
周睿 <zrufo747@outlook.com>
2025-05-28 11:59:16 +0800

    update

--- somehal-macros/src/lib.rs
+++ somehal-macros/src/lib.rs
@@ -206,8 +206,3 @@ pub fn aarch64_trap_handler(args: TokenStream, input: TokenStream) -> TokenStrea
         Aarch64TrapHandlerKind::SError => arch::aarch64::trap_handle_irq(func).into(),
     }
 }
-
-#[proc_macro]
-pub fn module_driver(input: TokenStream) -> TokenStream {
-    rdrive_macro_utils::module_driver_with_linker(input, "somehal::driver", None)
-}
